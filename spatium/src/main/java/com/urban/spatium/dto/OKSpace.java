e2550699987fb3702d2dc1da39bd23e160d0818b
ECS <ECS@DESKTOP-048HU3O>
2020-12-18 10:15:57 +0900

    공간 및 업체 추가 - 유선

---
+++ spatium/src/main/java/com/urban/spatium/dto/OKSpace.java
@@ -0,0 +1,5 @@
+package com.urban.spatium.dto;
+
+public class OKSpace {
+
+}
---
+++ spatium/src/main/java/com/urban/spatium/service/SpaceService.java
@@ -0,0 +1,8 @@
+package com.urban.spatium.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpaceService {
+
+}
