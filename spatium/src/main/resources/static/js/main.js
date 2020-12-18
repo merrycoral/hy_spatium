90f8a5af478edf8483e3b9f6963d0bb16ed23627
ECS <ECS@DESKTOP-048HU3O>
2020-12-18 12:11:56 +0900

    header 스크롤시 배경 바뀜 - 채현

--- spatium/src/main/resources/static/js/main.js
+++ spatium/src/main/resources/static/js/main.js
@@ -1,14 +1,29 @@
-	 $( document ).ready( function() {
+		$( document ).ready( function() {
 			const nav = document.querySelector('nav');
 		    const navTopOffset = nav.offsetTop;
-		    window.addEventListener('scroll', e => {
+		    $(document).scroll(function(){
 		      // nav를 상단에 고정. (브라우저 상단의 차이가 nav와 브라우저 상단의 차이보다 클 경우)
-		      if (window.pageYOffset >= navTopOffset) { 
-		        nav.classList.add('backColor')
-		        nav.classList.remove('color')
+		      if (window.pageYOffset > navTopOffset) { 
+		        nav.classList.add('backColor');
 		      } else {
-		        nav.classList.remove('backColor')
-		        nav.classList.add('color')
+		        nav.classList.remove('backColor');
 		      }
+		    	
 		    });
+		});
+		
+		$( document ).ready( function() {
+			var loginBtn = document.querySelector('.login-btn');
+			var login = document.querySelector('login');
+			var lineOne = document.querySelector('login .login-btn .line--1');
+			var lineTwo = document.querySelector('login .login-btn .line--2');
+			var lineThree = document.querySelector('login .login-btn .line--3');
+			var link = document.querySelector('login .login-links');
+			menuBtn.addEventListener('click', () => {
+				login.classList.toggle('login-open');
+			    lineOne.classList.toggle('line-cross');
+			    lineTwo.classList.toggle('line-fade-out');
+			    lineThree.classList.toggle('line-cross');
+			    link.classList.toggle('fade-in');
+			    
 		});
