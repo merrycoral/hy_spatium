f2203d9f09581944387d017e5d3217b7cb8d430b
ECS <ECS@DESKTOP-048HU3O>
2020-12-28 17:30:34 +0900

    회원가입, 정보수정 완료 -아영-

--- spatium/src/main/java/com/urban/spatium/mapper/UserMapper.java
+++ spatium/src/main/java/com/urban/spatium/mapper/UserMapper.java
@@ -9,14 +9,15 @@ import com.urban.spatium.dto.User;
 @Mapper
 public interface UserMapper {
 	
+	//회원삭제
+	int removeUser(String userId, String userLevel);
 	
 	//회원수정
 	int modifyUser(User user);
 	
 	//회원리스트
 	public List<User> getUserList();
-	
-	
+		
 	//로그인
 	public User login(String userId);
 	
