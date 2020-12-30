4acec2afabcd4b14c04dd311394d167052af7c3f
ECS <ECS@DESKTOP-048HU3O>
2020-12-30 17:30:29 +0900

    하영 페이징 소스 가져온거 적용 성공

--- spatium/src/main/java/com/urban/spatium/mapper/ReviewMapper.java
+++ spatium/src/main/java/com/urban/spatium/mapper/ReviewMapper.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.urban.spatium.Criteria;
 import com.urban.spatium.dto.Review;
 
 @Mapper
@@ -12,17 +13,22 @@ public interface ReviewMapper {
 
 	public List<Map<String, Object>> getAllReview(int startRow, int rowPerPage);
 
-	public List<Review> searchReview(String searchKey, String searchValue);
-
 	public int deleteReview(String reviewCode);
 
 	public int getAllReviewCount();
 
 	public int blindReview(String reviewCode, String blindValue);
 
-	public int getStoreReviewCount();
+	public int getStoreReviewCount(String sessionId);
 
 	public List<Map<String, Object>> getStoreReview(String sessionId, int startRow, int rowPerPage);
+
+
+	public List<Map<String, Object>> searchReview(int startRow, String searchKey, String searchValue);
+
+	public List<Review> exallReview(int startRow, int rowPerPage);
+
+
 
 
 }
