e2d4d962ff76ae4b758f3b774f54318613052e98
ECS <ECS@DESKTOP-048HU3O>
2021-01-05 12:06:21 +0900

    하영 - 01.05 관리자 정산 진행중

--- spatium/src/main/java/com/urban/spatium/controller/CalcController.java
+++ spatium/src/main/java/com/urban/spatium/controller/CalcController.java
@@ -1,17 +1,26 @@
 package com.urban.spatium.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.urban.spatium.dto.CalcWait;
+import com.urban.spatium.service.CalcService;
+
 
 @Controller
 public class CalcController {
+		@Autowired
+		private CalcService calcService;
 		
 		@GetMapping("/adminCalc")
 		public String adminCalc(Model model, @RequestParam(name="result", required = false) String result) {
+			List<CalcWait> calcWaitList = calcService.getCalcWait();
+			model.addAttribute("calcWaitList", calcWaitList);
 			return "calculate/adminCalc";
 		}
 		@GetMapping("/sellerCalc")
