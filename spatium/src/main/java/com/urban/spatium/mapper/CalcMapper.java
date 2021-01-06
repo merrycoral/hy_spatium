dcacc7eadc8f6e12760f834265caf44bfcc88e91
ECS <ECS@DESKTOP-048HU3O>
2021-01-06 17:24:23 +0900

    하영 - 01.06 - 실시간 매출, 일별 정산 추가

--- spatium/src/main/java/com/urban/spatium/mapper/CalcMapper.java
+++ spatium/src/main/java/com/urban/spatium/mapper/CalcMapper.java
@@ -3,12 +3,17 @@ package com.urban.spatium.mapper;
 import java.util.List;
 import java.util.Map;
 
-import com.urban.spatium.dto.CalcWait;
 
 public interface CalcMapper {
 
 	List<Map<String, Object>> getCalcWait(int startRow, int rowPerPage);
 
 	int getCalcWaitCount();
+
+	int getTodayCount(String today);
+
+	List<Map<String, Object>> getTodayList(String today);
+
+	List<Map<String, Object>> getTodaySubtotal(String today);
 
 }
