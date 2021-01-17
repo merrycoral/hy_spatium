e6df157436f9555a5e506729120313150179e655
Administrator <Administrator@192.168.123.101>
2021-01-17 22:52:52 +0900

    -태영 -01.17 환불페이지 결제페이지 오류수정

--- spatium/src/main/java/com/urban/spatium/controller/refundController.java
+++ spatium/src/main/java/com/urban/spatium/controller/refundController.java
@@ -2,9 +2,9 @@ package com.urban.spatium.controller;
 
 
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 import javax.servlet.http.HttpSession;
 
@@ -13,11 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.urban.spatium.dto.CancelRsv;
 import com.urban.spatium.dto.Payment;
@@ -101,11 +99,12 @@ public class refundController {
 	public String cancelTest(Model model
 							,@RequestParam(name="paymentCode", required = false) String paymentCode) {
 		
-		
+	
 		List<Payment> cacelData = paymentService.getPaymentCode(paymentCode);
 		List<Payment> payList = paymentService.rsvDetail(paymentCode);
 		int storeCode =payList.get(0).getOkayStoreCode();
 		System.out.println("스토어코드>>"+storeCode);
+		
 		List<RefundPolicy> refundPolicy =refundService.getRefundPolicy(storeCode);
 		System.out.println("cacelData 여기>>>>" + cacelData);
 		System.out.println("refundPolicy 여기2>>>>" + refundPolicy);
