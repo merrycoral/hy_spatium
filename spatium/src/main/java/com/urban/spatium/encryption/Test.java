01f9fa5016d6176fc2a24a0ee292f1325fdceff9
ECS <ECS@DESKTOP-048HU3O>
2021-01-28 14:56:29 +0900

    하영 - 01.28 - storeInfo.html 리뷰보임, 사장님 답글도 보임

--- spatium/src/main/java/com/urban/spatium/encryption/Test.java
+++ spatium/src/main/java/com/urban/spatium/encryption/Test.java
@@ -9,7 +9,7 @@ public class Test {
 		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor(); 
 		pbeEnc.setAlgorithm("PBEWithMD5AndDES"); 
 		pbeEnc.setPassword("ksmart37"); 
-		String url = "jdbc:log4jdbc:mysql://kissmekj.cafe24.com:3306/kissmekj?serverTimezone=UTC&characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull"; 
+		String url = "jdbc:log4jdbc:mysql://kissmekj.cafe24.com:3306/kissmekj?serverTimezone=Asia/Seoul&characterEncoding=UTF8&zeroDateTimeBehavior=convertToNull"; 
 		String username = "kissmekj"; 
 		String password = "nr09241844"; 
 		System.out.println("기존 URL :: " + url + " | 변경 URL :: " + pbeEnc.encrypt(url)); 
